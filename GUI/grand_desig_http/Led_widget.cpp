7ef91a40c3b490002bcfb6cdc301d45c6d9750a2
sqbbingo <2577200835@qq.com>
2019-01-07 18:11:14 +0800

    修改post前未清空数据的bug

--- GUI/grand_desig_http/Led_widget.cpp
+++ GUI/grand_desig_http/Led_widget.cpp
@@ -16,8 +16,8 @@ Led_Widget::Led_Widget(QWidget *parent) :
 
     //创建网络管理
     manager = new QNetworkAccessManager(this);
-    request.setRawHeader("api-key","Vt2rLag7l9LtcqUn7dT87psfxEY=");
-    request.setRawHeader("Host","api.heclouds.com");
+    request.setRawHeader("api-key","Vt2rLag7l9LtcqUn7dT87psfxEY=");//设置onenet密码
+    request.setRawHeader("Host","api.heclouds.com");//设置端口
     request.setHeader(QNetworkRequest::ContentTypeHeader, QVariant("text/plain"));//设置post的数据格式，同时防止打印出setheader的调试信息
 
     connect(wigt1,SIGNAL(StateChanged(int)),this,SLOT(wigt1sataeschanged(int)));//关联按钮的状态
@@ -52,11 +52,12 @@ void Led_Widget::on_comboBox_timing1_currentIndexChanged(int index)
     json_timing1.insert("timing1_state",index);
     json_timing1.insert("timing1_time",ui->timeEdit_timing1->time().toString());
     json_timing1.insert("/room/led1/state",ui->comboBox_timing1_aim->currentText());
-
-    post_data.append(QString(QJsonDocument(json_timing1).toJson()));
+    qDebug() <<json_timing1;
+    post_data.clear();
+    post_data.append(QString(QJsonDocument(json_timing1).toJson()));//将json格式转换为字符格式
     qDebug() << QString(QJsonDocument(json_timing1).toJson());
-    request.setUrl(QUrl("http://api.heclouds.com/mqtt?topic=/room/led1/timing1"));
-    manager->post(request,post_data);
+    request.setUrl(QUrl("http://api.heclouds.com/mqtt?topic=/room/led1/timing1"));//向onenet发送publish命令
+    manager->post(request,post_data);//发送post请求，设置
+    qDebug() << post_data;
 
-    qdbug() << "";
 }
