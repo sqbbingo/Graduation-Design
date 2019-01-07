79d7d9b19a0158895e0e5f80c518a59320740eb1
sqbbingo <2577200835@qq.com>
2019-01-07 16:25:51 +0800

    增加使用mqtt的代码

---
+++ GUI/GD_GUI_QT/gd_guii.pro
@@ -0,0 +1,40 @@
+#-------------------------------------------------
+#
+# Project created by QtCreator 2018-12-28T11:20:15
+#
+#-------------------------------------------------
+
+QT       += core gui
+QT       += network
+
+greaterThan(QT_MAJOR_VERSION, 4): QT += widgets
+
+TARGET = mqtt_onenet1
+TEMPLATE = app
+
+
+SOURCES += main.cpp\
+        mainwindow.cpp \
+    switchwidget.cpp
+
+HEADERS  += mainwindow.h \
+    mqtt/include/qmqtt/qmqttDepends \
+    mqtt/qmqtt.h \
+    switchwidget.h
+
+FORMS    += mainwindow.ui \
+    led.ui
+
+win32:CONFIG(release, debug|release): LIBS += -L$$PWD/lib/ -lqmqtt
+else:win32:CONFIG(debug, debug|release): LIBS += -L$$PWD/lib/ -lqmqttd
+else:unix: LIBS += -L$$PWD/lib/ -lqmqtt
+
+INCLUDEPATH += $$PWD/.
+DEPENDPATH += $$PWD/.
+
+win32:CONFIG(release, debug|release): LIBS += -L$$PWD/lib/ -lqmqtt
+else:win32:CONFIG(debug, debug|release): LIBS += -L$$PWD/lib/ -lqmqttd
+else:unix: LIBS += -L$$PWD/lib/ -lqmqtt
+
+INCLUDEPATH += $$PWD/.
+DEPENDPATH += $$PWD/.
---
+++ GUI/GD_GUI_QT/mainwindow.cpp
@@ -0,0 +1,114 @@
+#include "mainwindow.h"
+#include "ui_mainwindow.h"
+#include <QtNetwork>
+#include "mqtt/qmqtt.h"
+#include <QHostAddress>
+
+MainWindow::MainWindow(QWidget *parent) :
+    QMainWindow(parent),
+    ui(new Ui::MainWindow)
+{
+    ui->setupUi(this);
+    client = new QMQTT::Client();//初始化QMQTT客户端指针
+    ui->Host_comboBox->setEditable(true);
+    ui->Port_comboBox->setEditable(true);
+    ui->disconnect_pushButton->hide();
+
+    CSwitchWidget* wigt3 = new CSwitchWidget(this);
+    wigt3->SetPressStyle(GRAPH_PRESSSTYLE_SYSTEM2);
+    wigt3->setGeometry(QRect(50, 630, 150, 50));
+    wigt3->SetSwitchState(1);
+
+    connect(client,SIGNAL(connected()),this,SLOT(mqtt_connected()));
+    connect(client,SIGNAL(disconnected()),this,SLOT(mqtt_disconnected()));
+    connect(client,SIGNAL(received(const QMQTT::Message&)),this,SLOT(mqtt_receive_message(const QMQTT::Message&)));
+    connect(wigt3,SIGNAL(StateChanged(int)),this,SLOT(switch3sataechanged(int)));
+
+
+
+}
+
+MainWindow::~MainWindow()
+{
+    delete ui;
+}
+//与设置的mqtt服务器连接
+void MainWindow::on_connect_pushButton_clicked()
+{
+
+    QHostAddress host(ui->Host_comboBox->currentText());
+    QByteArray password = ui->IN_Password->text().toUtf8();
+//    QMQTT::MQTTVersion version;
+    quint16 port=(ui->Port_comboBox->currentText()).toUInt();
+
+    //client->set
+    client->setKeepAlive((ui->IN_Timeout->text()).toUInt());
+    client->setHost(host);//设置EMQ代理服务器IP
+    client->setPort(port);//设置EMQ代理服务器端口号
+    client->setClientId(ui->IN_Client_ID->text());//设置一个客户端ID
+    client->setUsername(ui->IN_Usernae->text());//设置一个客户端用户名
+    client->setPassword(password);//设置一个客户端密码
+    client->cleanSession();//清除缓存
+    if (ui->mqtt_version_comboBox->currentIndex() == 0)
+        client->setVersion(QMQTT::MQTTVersion::V3_1_1);//设置mqtt版本
+    else if (ui->mqtt_version_comboBox->currentIndex() == 1)
+        client->setVersion(QMQTT::MQTTVersion::V3_1_0);
+    client->connectToHost();//连接EMQ代理服务器
+
+    ui->connect_pushButton->hide();
+    ui->disconnect_pushButton->show();
+}
+//与mqtt服务器断开连接
+void MainWindow::on_disconnect_pushButton_clicked()
+{
+    client->disconnectFromHost();
+    ui->connect_pushButton->show();
+    ui->disconnect_pushButton->hide();
+}
+//发布主题
+void MainWindow::on_Pushlish_pushButton_clicked()
+{
+    QMQTT::Message mqtt_message(0,ui->Toplic_public_lineEdit->text(),ui->Public_message_textEdit->toPlainText().toUtf8(),ui->Public_Qos->currentIndex());
+    client->publish(mqtt_message);
+}
+//成功连接到mqtt服务器
+void MainWindow::mqtt_connected()
+{
+    ui->show_state_browser->insertPlainText("client had connected to broker\n");
+    ui->show_state_browser->moveCursor(QTextCursor::Start);
+}
+//与服务器断开
+void MainWindow::mqtt_disconnected()
+{
+    ui->show_state_browser->insertPlainText("lost connect\n");
+    ui->show_state_browser->moveCursor(QTextCursor::Start);
+}
+//接收到订阅的主题信息
+void MainWindow::mqtt_receive_message(const QMQTT::Message&message)
+{
+    QString strtopic = message.topic();
+    QString strpayload = message.payload();
+    ui->SubscribeMessage_Rece->insertPlainText(strtopic +":"+ strpayload + "\n");
+    ui->SubscribeMessage_Rece->moveCursor(QTextCursor::Start);
+}
+//订阅主题
+void MainWindow::on_Subscribe_pushButton_clicked()
+{
+    client->subscribe(ui->Subscribe_lineEdit->text(),0);
+}
+//按钮状态改变
+void MainWindow::switch3sataechanged(int i)
+{
+    qDebug() << i;
+    if(i == 1)
+    {
+        QMQTT::Message mqtt_message(0,"/room/led1/ON","ON",0);
+        client->publish(mqtt_message);
+    }
+    else if(i == 0)
+    {
+        QMQTT::Message mqtt_message(0,"/room/led1/OFF","OFF",0);
+        client->publish(mqtt_message);
+    }
+
+}
